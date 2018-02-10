283ab5b8710b2137335ccee8bcb8828ce598611e
Victor Monte <victorbcm@hotmail.com>
2018-02-09 23:27:28 -0200

    Generate migration to picture uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201140018) do
+ActiveRecord::Schema.define(version: 20180210012317) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180201140018) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
