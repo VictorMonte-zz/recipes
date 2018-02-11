fe0ad0325b0089138d6b845b4fb060b13a37462e
Victor Monte <victorbcm@hotmail.com>
2018-02-10 22:38:13 -0200

    Improving like feature
    Scope voting by recipe
    Descending recipe's votes in index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
   end
 
   def show
@@ -40,15 +40,20 @@ class RecipesController < ApplicationController
 
   def like
     @recipe = Recipe.find(params[:id])
-    Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
-    flash[:success] = "Your selection was successful"
-    redirect_to :back
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was successful"
+    else
+      flash[:danger] = "You can only like/dislike a recipe one "
+    end
+
+    redirect_to  :back
   end
 
   private
 
-    def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description, :picture)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :summary, :description, :picture)
+  end
 
 end
